0db4a7380e343d74596eb351e4263b3877251851
Saurabh Lakhanpal <firsty111@gmail.com>
2025-08-23 16:34:10 +0530

    issues fixed and unused code commented

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     react(),
     legacy()
   ],
-  //base: '/DiamondInventoryMobile/',
+  base: process.env.NODE_ENV === 'production' ? '/DiamondInventoryMobile/' : '/', 
   test: {
     globals: true,
     environment: 'jsdom',
