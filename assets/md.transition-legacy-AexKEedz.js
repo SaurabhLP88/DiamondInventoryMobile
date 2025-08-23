c7388a0761b532d9e7232497deb3b11a2b144922
Saurabh Lakhanpal <firsty111@gmail.com>
2025-08-23 18:13:15 +0530

    Updates

--- assets/md.transition-legacy-AexKEedz.js
+++ assets/md.transition-legacy-AexKEedz.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Dovejoz-.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-C_nF4Yxp.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
