e19d5c8375699068bd3c4ad4c4dd8c3709297232
Saurabh Lakhanpal <firsty111@gmail.com>
2025-08-23 18:00:47 +0530

    Updates

--- assets/ios.transition-legacy-C7fuvBcw.js
+++ assets/ios.transition-legacy-C7fuvBcw.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-yGazV8pw.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-Dovejoz-.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-B9R69Oh5.js
+++ assets/swipe-back-legacy-B9R69Oh5.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-yGazV8pw.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-Dovejoz-.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
