4df0b7b58dd2b397c33c5b0d0ab08c67dd288f43
Saurabh Lakhanpal <firsty111@gmail.com>
2025-08-23 17:22:13 +0530

    Updates

--- assets/ios.transition-legacy-D-9NdSLg.js
+++ assets/ios.transition-legacy-D-9NdSLg.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BoQ_ouEq.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-dNU6OEV7.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
